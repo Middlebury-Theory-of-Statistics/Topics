3fb4bb1fdbffcef8fb7f298b1eba29b46f17f6bb
rudeboybert <albert.ys.kim@gmail.com>
2016-02-23 22:50:18 -0500

    Done example of non-symmetric null dist'n.

--- Lec04 Contingency Tables/Ex_3-3.R
+++ Lec04 Contingency Tables/Ex_3-3.R
@@ -30,38 +30,3 @@ hist(result, xlab = x_label, breaks=50, main=title, xlim=c(-20, 10))
 abline(v = observed, col = "blue", lty=5)
 
 (sum(result <= observed)+1)/(B + 1)  #P-value
-
-
-
-
-
-# Different p-values ------------------------------------------------------
-# Say instead the observed test statistic was -4.5
-observed <- -4.5
-hist(result, xlab = x_label, breaks=50, main=title, xlim=c(-20, 10))
-abline(v = observed, col = "blue", lty=5)
-
-# One-sided p-value corresponding to HA: mu1 < mu2
-(sum(result <= observed)+1)/(B + 1)
-
-
-# We have three possible two-sided p-values corresponding to HA: mu1 != mu2
-
-# Method 1: Double the one-sided p-value despite the fact reference distribution
-# is not symmetric
-2 * (sum(result <= observed) + 1)/(B + 1)
-
-
-# Method 2: More extreme on the domain
-hist(result, xlab = x_label, breaks=50, main=title)
-abline(v = c(observed, -observed), col = "blue", lty=5)
-
-# Area to the left of -4.5 and to the right of 4.5
-(sum(result <= observed) + sum(result >= -observed) + 1)/(B + 1) 
-
-
-# Method 3: More extreme on the range
-hist(result, xlab = x_label, breaks=50, main=title)
-abline(v = observed, col = "blue", lty=5)
-abline(h=169, col="red", lty=5)
-abline(v=)
--- README.md
+++ README.md
@@ -7,6 +7,8 @@ The course syllabus can be found [here](http://rudeboybert.github.io/pages/teach
 
 * **Lec04: Chi-Square Test Statistic and Contingency Tables**
     + <a href="http://htmlpreview.github.io/?https://raw.githubusercontent.com/Middlebury-Theory-of-Statistics/Topics/master/Lec04%20Contingency%20Tables/Lec04.html#1" target="_blank">Slides</a>
+    + Files:
+        * Code for Example 3.3 on page 44 of a non-symmetric null distribution: `Ex_3-3.R`
 * **Lec03: Permutation Tests**
     + <a href="http://htmlpreview.github.io/?https://raw.githubusercontent.com/Middlebury-Theory-of-Statistics/Topics/master/Lec03%20Permutation%20Test/Lec03.html" target="_blank">Slides</a>
 
